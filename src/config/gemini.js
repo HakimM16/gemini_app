f531a573326737206feb336fd911a6c3652a2df6
Hakim Mabike <hakimmabike2@gmail.com>
2025-02-03 17:29:18 +0000

    Changed the gemini API model

--- src/config/gemini.js
+++ src/config/gemini.js
@@ -8,7 +8,7 @@ import {
   const genAI = new GoogleGenerativeAI(apiKey);
   
   const model = genAI.getGenerativeModel({
-    model: "gemini-pro",
+    model: "gemini-2.0-flash-exp",
   });
   
   const generationConfig = {
