2772b34d6cf5c7cd99d6feeaebd1b384ea6b171e
Hakim Mabike <hakimmabike2@gmail.com>
2025-02-03 16:43:02 +0000

    Added a transition feature for the sidebar

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -14,7 +14,7 @@ const Sidebar = () => {
     }
 
   return (
-    <div className='sidebar'>
+    <div className={`sidebar ${extended ? 'extended' : ''}`}>
         <div className="top">
             <img onClick={() => setExtended(prev => !prev)} src={assets.menu_icon} alt='' />
             <div onClick={() => newChat()} className="new-chat">
